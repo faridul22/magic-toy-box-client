8ebce2d70138bc70c4ef4556eb8900c617ddb2cf
faridul22 <96929890+faridul22@users.noreply.github.com>
2023-05-23 18:02:06 +0600

    home page responsive done and subscribe section added

--- src/pages/Home/PeopleUse/PeopleUse.jsx
+++ src/pages/Home/PeopleUse/PeopleUse.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const PeopleUse = () => {
     return (
-        <div className='bg-base-200 py-20 my-28 md:w-full sm:w-full container mx-auto rounded-xl'>
+        <div className='bg-base-200 py-20 my-28 sm:w-full mx-auto rounded-xl'>
             <h2 className='lg:text-4xl font-semibold text-center mb-10'>Millions of people use: Magic ToyBox</h2>
-            <div className='w-3/4 mx-auto grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4'>
-                <div className="card w-4/5 lg:w-60 lg:h-60 md:mx-4 sm:mx-3 mr-10 bg-green-200 shadow-lg">
+            <div className='ml-5 mx-auto grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 lg:gap-0 gap-4'>
+                <div className="card mx-auto w-4/5 lg:w-60 lg:h-60 sm:mx-3 mr-10 bg-green-200 shadow-lg">
                     <figure className="pt-10">
                         <img width={80} src="https://i.ibb.co/YTn8HkS/free.png" alt="free shipping" className="rounded-lg" />
                     </figure>
@@ -13,7 +13,7 @@ const PeopleUse = () => {
                         <h2 className="card-title">Free Shipping</h2>
                     </div>
                 </div>
-                <div className="card w-4/5 lg:w-60 lg:h-60 md:mx-4 sm:mx-3 bg-base-100 mr-10 shadow-lg">
+                <div className="card mx-auto w-4/5 lg:w-60 lg:h-60 sm:mx-3 bg-base-100 mr-10 shadow-lg">
                     <figure className="pt-10">
                         <img width={80} src="https://i.ibb.co/GPMNQfD/moneyback-1-1.png" alt="free shipping" className="" />
                     </figure>
@@ -21,7 +21,7 @@ const PeopleUse = () => {
                         <h2 className="card-title">Money Back</h2>
                     </div>
                 </div>
-                <div className="card w-4/5 lg:w-60 lg:h-60 md:mx-4 sm:mx-3 bg-yellow-100 mr-10 shadow-lg">
+                <div className="card mx-auto w-4/5 lg:w-60 lg:h-60 sm:mx-3 bg-yellow-100 mr-10 shadow-lg">
                     <figure className="pt-10">
                         <img width={80} src="https://i.ibb.co/HrfhndC/support-icon-1-1.png" alt="free shipping" className="" />
                     </figure>
--- src/pages/Home/SubscribeSection/SubscribeSection.jsx
+++ src/pages/Home/SubscribeSection/SubscribeSection.jsx
@@ -6,10 +6,10 @@ const SubscribeSection = () => {
         <div className='my-32'>
 
             <div className="card h-80 container md:flex mx-auto lg:card-side bg-[#52C1BA] shadow-xl">
-                <figure><img width={400} src="https://i.ibb.co/Yfh8Czt/newsletters-1.png" alt="Album" /></figure>
+                <figure><img width={400} className='hidden lg:block' src="https://i.ibb.co/Yfh8Czt/newsletters-1.png" alt="Album" /></figure>
                 <div className="card-body">
                     <div className='text-center '>
-                        <h2 className="text-7xl font-medium mb-3">Subscribe</h2>
+                        <h2 className="text-7xl font-medium mb-3 text-gray-800">Subscribe</h2>
                         <span className='text-2xl text-gray-700 mt-5'>Subscribe to keep up with fresh news and existing updates every moment.</span>
                     </div>
                     <div className="card-actions my-auto justify-center">
@@ -31,19 +31,3 @@ const SubscribeSection = () => {
 };
 
 export default SubscribeSection;
-
-/* <div className="card-actions justify-center">
-                        <div className='text-black'>
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Enter amount</span>
-                                </label>
-                                <label className="input-group">
-                                    <input type="text" placeholder="0.01" className="input input-bordered" />
-                                    <span>BTC</span>
-                                </label>
-                            </div>
-                        </div>
-                    </div> */
